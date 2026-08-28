25534829786a7ebfa9b6fd1cf42e97374c1c6f0f
Ace Marino <aislinnm@buffalo.edu>
2026-08-28 08:34:17 -0400

    Update SectionMeeting Testing.sql

--- Working Sqls Ace/SectionMeeting Testing.sql
+++ Working Sqls Ace/SectionMeeting Testing.sql
@@ -1,11 +1,27 @@
 --21978 and 23932
-select * from ps_rpt.cmp_section_meeting_v where course_ref_no='23932' and term_code='2269';
-select * from ps_rpt.cmp_section_v where course_number='101' and subject_cd='MGG' and term_code='2269' and course_type_code='REC';
+select * from ps_rpt.cmp_section_meeting_v where course_ref_no='21969' and term_code='2269';
+select * from ps_rpt.cmp_section_meeting_v where course_ref_no='21781' and term_code='2269';
+select * from ps_rpt.cmp_section_v where course_number='101' and subject_cd='MGG' and term_code='2269' and course_type_code='REC' and section_name='A1' ;
+
+select * from ps_rpt.cmp_section_meeting_v where course_ref_no='21978' and term_code='2269';
+select * from ps_rpt.cmp_section_v where course_number='101' and subject_cd='MGG' and term_code='2269' and course_type_code='REC' and section_name='C1' ;
+
+
 select * from ps_rpt.cmp_osf_cs_courses_v where subject_cd='MGG' and course_no='101';
 select * from ps_rpt.ps_class_tbl where subject='MGG' and CATALOG_NBR =' 101LR';
 
+select * from ps_rpt.cmp_section_v where term_code='2269' and course_ref_no='10002';
+select * from ps_rpt.cmp_section_meeting_v where term_code='2269' and course_ref_no='10002';
+select * from ps_rpt.cmp_osf_cs_courses_v where subject_cd='LAI' and course_no='640';
+
+select * from ps_rpt.ps_class_tbl where subject='CHE' and strm='2269';
+SELECT TO_CHAR
+    (SYSDATE, 'MM-DD-YYYY ') "NOW"
+     FROM DUAL;
+select * from ps_rpt.cmp_section_v;
 select class_stat from ps_rpt.ps_class_tbl where CLASS_STAT <> 'X';
-select class_stat from ps_rpt.ps_class_tbl where CLASS_STAT = 'I';
+select distinct class_stat from ps_rpt.ps_class_tbl; 
+
 select count(*) from ps_rpt.cmp_section_meeting_v;
 --base
 SELECT NVL (c.STRM, '')
@@ -43,8 +59,8 @@ SELECT NVL (c.STRM, '')
              mtg.FACILITY_ID
                  AS LOCATION_CD,
              CASE
-                 WHEN     (   (mtg.START_DT > SYSDATE)
-                           OR (SYSDATE BETWEEN mtg.START_DT AND mtg.END_DT))
+                 WHEN     (   (mtg.START_DT > TRUNC(SYSDATE - 3) )
+                           OR (TRUNC(SYSDATE - 3)  BETWEEN mtg.START_DT AND mtg.END_DT))
                       AND c.CLASS_STAT <> 'X'
                  THEN
                      'Y'
@@ -93,7 +109,7 @@ SELECT NVL (c.STRM, '')
                     
        WHERE     1 = 1
              AND C.ENRL_TOT > 0
-              and c.CLASS_NBR ='23932' and c.STRM='2269'
+              and c.CLASS_NBR ='21841' and c.STRM='2269'
              AND c.STRM >= (SELECT cf.lookback_term --Rolling filter to grab only terms up to a year ago
                               FROM ps_rpt.cmp_filter_current_v cf)
                             
@@ -197,3 +213,132 @@ SELECT NVL (c.STRM, '')
 --tie active flag to class status
 
 --tied to term dates(this is bad)
+
+select TRUNC(SYSDATE - 3);
+--section
+WITH Section
+        AS (  SELECT NVL (TRIM (a.CLASS_SECTION), '') AS SECTION_NAME,
+                     NVL (TRIM (REGEXP_SUBSTR (a.catalog_nbr, '\d+')), '')
+                        AS COURSE_NUMBER,
+                     NVL (TRIM (a.SUBJECT), '') AS SUBJECT_CD,
+                     NVL (a.CLASS_NBR, '') AS COURSE_REF_NO,
+                     NVL (a.STRM, '') AS TERM_CODE,
+                     NVL (a.SSR_COMPONENT, '') AS COURSE_TYPE_CODE,
+                     ' ' AS SECTION_TAG,
+                        a.subject
+                     || ' '
+                     || a.catalog_nbr
+                     || ' '
+                     || NVL (a.DESCR, '')
+                        AS SECTION_TITLE,
+                     N.Name AS Instructor_Name,
+                     B.start_dt AS Class_Start_Dt,
+                     B.end_dt AS Class_End_Dt,
+                     'N' AS is_unlimited_seating,
+                     ' ' AS section_type,
+                     CASE WHEN B.end_dt >= TRUNC(SYSDATE - 3) THEN 'Y' ELSE 'N' END
+                        AS is_active
+                FROM ps_rpt.PS_CLASS_TBL a
+                     LEFT JOIN ps_rpt.PS_CLASS_MTG_PAT B
+                        ON     B.CRSE_ID = A.CRSE_ID
+                           AND B.CRSE_OFFER_NBR = A.CRSE_OFFER_NBR
+                           AND B.STRM = A.STRM
+                           AND B.SESSION_CODE = A.SESSION_CODE
+                           AND B.CLASS_SECTION = A.CLASS_SECTION
+                     LEFT JOIN (SELECT *
+                                  FROM PS_RPT.PS_CLASS_INSTR
+                                 WHERE INSTR_ROLE IN ('PI',
+                                                      'SI',
+                                                      'TA',
+                                                      'CA')) I
+                        ON (    a.CRSE_ID = I.CRSE_ID
+                            AND a.STRM = I.STRM
+                            AND a.CRSE_OFFER_NBR = I.CRSE_OFFER_NBR
+                            AND a.SESSION_CODE = I.SESSION_CODE
+                            AND a.CLASS_SECTION = I.CLASS_SECTION)
+                     LEFT JOIN
+                     (SELECT NA.EMPLID, NA.Name, NA.NAME_TYPE
+                        FROM PS_RPT.PS_NAMES NA
+                       WHERE     NA.NAME_TYPE =
+                                    CASE
+                                       WHEN EXISTS
+                                               (SELECT *
+                                                  FROM PS_RPT.PS_NAMES NMPRF
+                                                 WHERE     NAME_TYPE = 'PRF'
+                                                       AND EFF_STATUS = 'A'
+                                                       AND NA.EMPLID =
+                                                              NMPRF.EMPLID
+                                                       AND NMPRF.EFFDT =
+                                                              (SELECT MAX (
+                                                                         NMPRF_1.EFFDT)
+                                                                 FROM PS_RPT.PS_NAMES
+                                                                      NMPRF_1
+                                                                WHERE     NMPRF.EMPLID =
+                                                                             NMPRF_1.EMPLID
+                                                                      AND NMPRF.NAME_TYPE =
+                                                                             NMPRF_1.NAME_TYPE
+                                                                      AND NMPRF_1.EFFDT <=
+                                                                             SYSDATE))
+                                       THEN
+                                          'PRF'
+                                       ELSE
+                                          'PRI'
+                                    END
+                             AND NA.EFFDT =
+                                    (SELECT MAX (B_ED.EFFDT)
+                                       FROM PS_RPT.PS_NAMES B_ED
+                                      WHERE     NA.EMPLID = B_ED.EMPLID
+                                            AND NA.NAME_TYPE = B_ED.NAME_TYPE
+                                            AND B_ED.EFFDT <= SYSDATE)
+                             AND NA.EFF_STATUS = 'A') N
+                        ON (I.EMPLID = N.EMPLID)
+               WHERE     1 = 1
+                     AND a.enrl_tot > 0
+                     AND a.STRM >= (SELECT cf.lookback_term --Rolling filter to grab only terms up to a year ago
+                                      FROM ps_rpt.cmp_filter_current_v cf)
+                                      and a.CLASS_NBR ='21841' and a.STRM='2269'
+            ORDER BY a.subject, a.catalog_nbr)
+     SELECT SECTION_NAME,
+            COURSE_NUMBER,
+            SUBJECT_CD,
+            COURSE_REF_NO,
+            TERM_CODE,
+            COURSE_TYPE_CODE,
+            SECTION_TAG,
+            SECTION_TITLE,
+            SUBSTR(LISTAGG (INSTRUCTOR_NAME, '; ')
+               WITHIN GROUP (ORDER BY INSTRUCTOR_NAME), 1, 64) --Adding this to concatenate instructor names on one row
+               AS INSTRUCTOR_NAME,
+            CLASS_START_DT,
+            CLASS_END_DT,
+            IS_UNLIMITED_SEATING,
+            SECTION_TYPE,
+            IS_ACTIVE
+       FROM (SELECT DISTINCT INSTRUCTOR_NAME,
+                             SECTION_NAME,
+                             COURSE_NUMBER,
+                             SUBJECT_CD,
+                             COURSE_REF_NO,
+                             TERM_CODE,
+                             COURSE_TYPE_CODE,
+                             SECTION_TAG,
+                             SECTION_TITLE,
+                             CLASS_START_DT,
+                             CLASS_END_DT,
+                             IS_UNLIMITED_SEATING,
+                             SECTION_TYPE,
+                             IS_ACTIVE
+               FROM Section)
+   GROUP BY SECTION_NAME,
+            COURSE_NUMBER,
+            SUBJECT_CD,
+            COURSE_REF_NO,
+            TERM_CODE,
+            COURSE_TYPE_CODE,
+            SECTION_TAG,
+            SECTION_TITLE,
+            CLASS_START_DT,
+            CLASS_END_DT,
+            IS_UNLIMITED_SEATING,
+            SECTION_TYPE,
+            IS_ACTIVE;
